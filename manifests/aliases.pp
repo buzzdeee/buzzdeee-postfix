aa788d1751a44576be852cf8b9c54e23f1d94621
Sebastian Reitenbach <sebastian.reitenbach@blackbridge.com>
2015-03-27 08:49:20 +0100

    Add missing aliases class

---
+++ manifests/aliases.pp
@@ -0,0 +1,22 @@
+# private class, do not use directly
+# takes care to setup the aliases file
+
+class postfix::aliases (
+  $aliases,
+  $alias_map,
+  $newaliases,
+) {
+  each ($aliases) |$origrecipient, $recipient| {
+    mailalias { $origrecipient:
+      recipient => $recipient,
+      target    => $alias_map,
+      notify    => Exec['update mail aliases'],
+    }
+  }
+
+  exec { 'update mail aliases':
+    command     => $newaliases,
+    refreshonly => true,
+  }
+
+}
